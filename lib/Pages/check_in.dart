6f34694b79abfc0248d1b1ffffe44e356b4d89dc
ocean-rey <x7flux@gmail.com>
2021-11-25 00:39:10 +0300

    fixed some issues

--- lib/Pages/check_in.dart
+++ lib/Pages/check_in.dart
@@ -3,23 +3,63 @@ import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 
 class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+
   @override
   _DashboardState createState() => _DashboardState();
 }
 
 class _DashboardState extends State<DashboardScreen> {
-  final Geolocator geolocator = Geolocator()
-    ..forceAndroidLocationManager;
-   Position _currentPosition;
-   String _currentAddress;
+  // you don't need to create an instance of Geolocator; its functions are static
+  // final Geolocator geolocator = Geolocator();
+  // you also don't need these but i see where you're going with this.
+  late Position _currentPosition;
+  // late String _currentAddress;
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
 
   @override
   void initState() {
     super.initState();
-    _getCurrentLocation();
+    _currentPosition = _determinePosition() as Position;
   }
 
-  _getCurrentLocation() {
+/*   _getCurrentLocation() {
     geolocator
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
         .then((Position position) {
@@ -29,11 +69,12 @@ class _DashboardState extends State<DashboardScreen> {
 
       _getAddressFromLatLng();
     }).catchError((e) {
+      // ignore: avoid_print
       print(e);
     });
-  }
+  } */
 
-  _getAddressFromLatLng() async {
+  /*  _getAddressFromLatLng() async {
     try {
       List<Placemark> p = await geolocator.placemarkFromCoordinates(
           _currentPosition.latitude, _currentPosition.longitude);
@@ -42,12 +83,12 @@ class _DashboardState extends State<DashboardScreen> {
 
       setState(() {
         _currentAddress =
-        "${place.locality}, ${place.postalCode}, ${place.country}";
+            "${place.locality}, ${place.postalCode}, ${place.country}";
       });
     } catch (e) {
       print(e);
     }
-  }
+  } */
 
   @override
   Widget build(BuildContext context) {
@@ -79,12 +120,13 @@ class _DashboardState extends State<DashboardScreen> {
                   decoration: BoxDecoration(
                     color: Theme.of(context).canvasColor,
                   ),
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                   child: Column(
                     children: <Widget>[
                       Row(
                         children: <Widget>[
-                          Icon(Icons.location_on),
+                          const Icon(Icons.location_on),
                           const SizedBox(
                             width: 8,
                           ),
@@ -96,11 +138,15 @@ class _DashboardState extends State<DashboardScreen> {
                                   'Location',
                                   style: Theme.of(context).textTheme.caption,
                                 ),
-                                if (_currentPosition != null &&
-                                    _currentAddress != null)
-                                  Text(_currentAddress,
-                                      style:
-                                      Theme.of(context).textTheme.bodyText2),
+                                Text(
+                                    "Lat: " +
+                                        _currentPosition.latitude.toString() +
+                                        "\nLong: " +
+                                        _currentPosition.longitude.toString() +
+                                        "\nAccuracy: " +
+                                        _currentPosition.accuracy.toString(),
+                                    style:
+                                        Theme.of(context).textTheme.bodyText2),
                               ],
                             ),
                           ),
