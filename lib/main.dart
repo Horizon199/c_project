9daf15b7877515d1031313dc59d22415cb375bfc
Horizon199 <shoib37@gmail.com>
2021-11-26 00:57:29 +0300

    GJ SULTAN U R THE BEST

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Checking(),
+      home: LoginScreen(),
     );
   }
 }
