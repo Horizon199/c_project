6f7c4faf84dd69f2ed0a35d22d150afd1a637436
ocean-rey <x7flux@gmail.com>
2021-11-26 03:00:30 +0300

    working event fetching + display

--- lib/utilities/expansion_tile_card_demo.dart
+++ lib/utilities/expansion_tile_card_demo.dart
@@ -1,7 +1,21 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:expansion_tile_card/expansion_tile_card.dart';
 
 class ExpansionTileCardDemo extends StatefulWidget {
+  final String title;
+  final String desc;
+  final String imageUrl;
+
+  const ExpansionTileCardDemo(
+      {Key? key,
+      required this.title,
+      required this.desc,
+      required this.imageUrl})
+      : super(key: key);
 
   @override
   _ExpansionTileCardDemoState createState() => _ExpansionTileCardDemoState();
@@ -13,96 +27,74 @@ class _ExpansionTileCardDemoState extends State<ExpansionTileCardDemo> {
   @override
   Widget build(BuildContext context) {
     return Wrap(
-        spacing: 8.0, // gap between adjacent chips
-        runSpacing: 4.0, // gap between lines
-        children:  <Widget>[
-            ExpansionTileCard(
-              baseColor: Colors.blue,
-              expandedColor: Colors.white,
-              key: cardA,
-              leading:Image.network('https://picsum.photos/250?image=9', fit: BoxFit.cover),
-              title: const Text("Flutter Dev's"),
-              subtitle: const Text("FLUTTER DEVELOPMENT COMPANY"),
+      spacing: 8.0, // gap between adjacent chips
+      runSpacing: 4.0, // gap between lines
+      children: <Widget>[
+        ExpansionTileCard(
+          baseColor: Colors.blue,
+          expandedColor: Colors.white,
+          key: cardA,
+          leading: Image.network(widget.imageUrl, fit: BoxFit.cover),
+          title: Text(widget.title),
+          children: <Widget>[
+            const Divider(
+              thickness: 1.0,
+              height: 1.0,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 8.0,
+                ),
+                child: Text(widget.desc,
+                    style: Theme.of(context).textTheme.bodyText2),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 8.0,
+                  ),
+                  child: Image(
+                    image: NetworkImage(widget.imageUrl),
+                  )),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceAround,
+              buttonHeight: 52.0,
+              buttonMinWidth: 90.0,
               children: <Widget>[
-                const Divider(
-                  thickness: 1.0,
-                  height: 1.0,
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0,
-                      vertical: 8.0,
-                    ),
-                    child: Text(
-                        "FlutterDevs specializes in creating cost-effective and efficient applications with our perfectly crafted,"
-                            " creative and leading-edge flutter app development solutions for customers all around the globe.",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText2
-                    ),
+                FlatButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4.0)),
+                  onPressed: () {
+                    // do checkin
+                    cardA.currentState?.expand();
+                  },
+                  child: Column(
+                    children: const <Widget>[
+                      Icon(Icons.arrow_downward),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 2.0),
+                      ),
+                      Text('Checkin'),
+                    ],
                   ),
-                ),
-                const Align(
-                  alignment: Alignment.center,
-                  child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 16.0,
-                        vertical: 8.0,
-                      ),
-                      child: Image(
-                        image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                      )
-                  ),
-                ),
-                ButtonBar(
-                  alignment: MainAxisAlignment.spaceAround,
-                  buttonHeight: 52.0,
-                  buttonMinWidth: 90.0,
-                  children: <Widget>[
-                    FlatButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4.0)),
-                      onPressed: () {
-                        cardA.currentState?.expand();
-                      },
-                      child: Column(
-                        children: const <Widget>[
-                          Icon(Icons.arrow_downward),
-                          Padding(
-                            padding: EdgeInsets.symmetric(vertical: 2.0),
-                          ),
-                          Text('Open'),
-                        ],
-                      ),
-                    ),
-                    FlatButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4.0)),
-                      onPressed: () {
-                        cardA.currentState?.collapse();
-                      },
-                      child: Column(
-                        children: const <Widget>[
-                          Icon(Icons.arrow_upward),
-                          Padding(
-                            padding: EdgeInsets.symmetric(vertical: 2.0),
-                          ),
-                          Text('Close'),
-                        ],
-                      ),
-                    ),
-                  ],
                 ),
               ],
             ),
-          const Divider(
-            thickness: 1.0,
-            height: 1.0,
-            color: Colors.white,
-          ),
-        ],
-      );
+          ],
+        ),
+        const Divider(
+          thickness: 1.0,
+          height: 1.0,
+          color: Colors.white,
+        ),
+      ],
+    );
   }
 }
